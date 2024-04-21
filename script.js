00805411628335e037aabc5392116fc9977f2a42
LuLuMS0206 <luciamacho00@gmail.com>
2024-04-21 17:36:49 +0200

    firstcommitJS

---
+++ script.js
@@ -0,0 +1,12 @@
+const changeNavbar = document.getElementById("changeNavbar");
+const navbar = document.getElementById("navbar");
+
+changeNavbar.addEventListener("click", (event) => {
+    if (navbar.style.display === "none") {
+        navbar.style.display = "block";
+        console.log(changeNavbar)
+        console.log(navbar)
+    } else {
+        navbar.style.display = "none";
+    }
+});
