9a50a1e856f05fe2dd8f3ea1320da0d352f33ed4
LuLuMS0206 <luciamacho00@gmail.com>
2024-04-22 21:03:30 +0200

    end create scroll

--- script.js
+++ script.js
@@ -1,19 +1,35 @@
 //menu desplegable
-const changeNavbar = document.getElementById("changeNavbar");
-const navbar = document.getElementById("navbar");
+
+
+// changeNavbar.addEventListener("click", (event) => {
+//     if (navbar.style.display === "none") {
+//         navbar.style.display = "block";
+//         console.log(changeNavbar)
+//         console.log(navbar)
+//     } else {
+//         navbar.style.display = "none";
+//     }
+// });
 
 changeNavbar.addEventListener("click", (event) => {
-    if (navbar.style.display === "none") {
+    const changeNavbar = document.getElementById("changeNavbar");
+    const navbar = document.getElementById("navbar");
+    const computedStyle = window.getComputedStyle(navbar);
+    const displayStyle = computedStyle.getPropertyValue("display");
+
+    console.log(computedStyle)
+    console.log(displayStyle)
+    if (displayStyle === "none") {
         navbar.style.display = "block";
-        console.log(changeNavbar)
-        console.log(navbar)
+        console.log("El estilo de 'navbar' era 'none' y se cambió a 'block'");
     } else {
         navbar.style.display = "none";
+        console.log("El estilo de 'navbar' era diferente de 'none' y se cambió a 'none'");
     }
 });
 
 //boton go home
-const button_scroll = document.getElementById("button__go_home");
+const button_scroll = document.getElementById("btn_scroll");
 const body = document.getElementById("body");
 
 button_scroll.addEventListener("click", () => {
@@ -24,3 +40,15 @@ button_scroll.addEventListener("click", () => {
         });
     }, 200);
 });
+
+//scroll
+window.addEventListener('load', () => {
+    const scrollerProgress = document.getElementById('scrollerProgress');
+    requestAnimationFrame(scroll);
+}
+)
+
+function scroll () {
+    scrollerProgress.style.width = `${((window.scrollY) / (document.body.scrollHeight - window.innerHeight)*100)}%`;
+    requestAnimationFrame(scroll);
+}
