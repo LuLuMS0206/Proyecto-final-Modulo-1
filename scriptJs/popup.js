0be60fff810b16c3c4773f1f548bc172ec3f1452
LuLuMS0206 <luciamacho00@gmail.com>
2024-04-29 11:37:11 +0200

    localstore

--- scriptJs/popup.js
+++ scriptJs/popup.js
@@ -4,11 +4,28 @@
 const close = document.getElementById("close");
 const contentPopup = document.getElementById("content-popup");
 
-//mostrar el popup despues de 5 segundos
+
+let showPopup = localStorage.getItem("showPopup"); 
+
+// Mostrar el popup después de 5 segundos 
 setTimeout(() => {
-    contentPopup.style.display = 'initial' 
+    if (showPopup !== "false") {
+        contentPopup.style.display = 'initial'; 
+        localStorage.setItem("showPopup", "false"); 
+    }
 }, 5000);
 
+
+
+// //mostrar el popup cuando se haya desplazado al 25% de la página
+
+window.addEventListener("scroll", () => {
+    if (window.scrollY / document.documentElement.scrollHeight >= 0.25 && localStorage.getItem("showPopup") !== "true") {
+        showPopup.classList.add("opened");
+
+        localStorage.setItem("showPopup", "true");
+    }
+})
 
 
 //cierre del popup con close
@@ -25,7 +42,7 @@ document.addEventListener("keydown", (event) => {
 });
 
 
-//validar formulario del popup <script>
+//validar formulario del popup 
 
     const nameInput = document.getElementById("popupNameInput");
     const emailInput = document.getElementById("popupEmailInput");
@@ -47,6 +64,6 @@ document.addEventListener("keydown", (event) => {
 
     // Función para validar una dirección de correo electrónico
     function isValidEmail(email) {
-        const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+        const emailRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
         return emailRegex.test(email);
     }
